ccae29d5d38ca2af97e799d1457227f25b11e17b
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-02-05 10:47:09 +0200

    Reformat code

--- app/src/main/java/com/example/rollerdice/MainActivity.kt
+++ app/src/main/java/com/example/rollerdice/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.rollerdice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -13,22 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text=getString(R.string.lets_roll)
-        rollButton.setOnClickListener{
-          rollDice()
+        rollButton.text = getString(R.string.lets_roll)
+        rollButton.setOnClickListener {
+            rollDice()
         }
     }
 
     private fun rollDice() {
-        val drawableResource= when (Random.nextInt(6)+1){
-            1-> R.drawable.dice_1
-            2-> R.drawable.dice_2
-            3-> R.drawable.dice_3
-            4-> R.drawable.dice_4
-            5-> R.drawable.dice_5
-            else-> R.drawable.dice_6
+        val drawableResource = when (Random.nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
-        val diceImage: ImageView =findViewById(R.id.dice_image)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }
